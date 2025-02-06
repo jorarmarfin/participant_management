af78c7aab057271554749b27dedd085ded82d10d
Luis Mayta <luis.mayta@gmail.com>
2025-02-05 22:30:46 -0500

    quitar dd

--- app/Services/WaveConnectedService.php
+++ app/Services/WaveConnectedService.php
@@ -17,7 +17,6 @@ class WaveConnectedService
             ];
             // Realizar la solicitud HTTP
             $uri = env('WAVECONNECTED_URL').'/api';
-            dd($method, $uri . $url, $options);
             $response = $client->request($method, $uri . $url, $options);
             // Decodificar la respuesta JSON a un array de PHP
             return json_decode($response->getBody(), true);
