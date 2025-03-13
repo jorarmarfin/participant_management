2621a1400dafe8e167a08feccb8c72aaaab54eea
Luis Mayta <luis.mayta@gmail.com>
2025-03-12 21:32:35 -0500

    cambio de grid

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                     <p class="mt-6 text-gray-500 leading-relaxed">
                         Sistema diseñado para gestionar el registro y seguimiento de participantes en eventos organizados por ONGs, instituciones educativas o comunidades. Su objetivo principal es facilitar la fidelización de los asistentes y proporcionar herramientas para un análisis estadístico eficiente.
                     </p>
-                    <div class="grid grid-cols-5 gap-4 mt-8">
+                    <div class="grid grid-cols-4 gap-4 mt-8">
                         <livewire:dashboard.card-attached-live :count="$count_participants" name="Participantes" />
                         <livewire:dashboard.card-attached-live :count="$count_attached" name="Adherido" />
                         <livewire:dashboard.card-attached-live :count="$count_new" name="Nuevo (Charla)" />
